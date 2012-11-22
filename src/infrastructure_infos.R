73482baf468f0f786b30379862dba9434f277980
Gyorgy Nadudvari <reedcourty@gmail.com>
2012-11-22 10:58:03 +0100

    Az infrastruktúra infókat tartalmazó modul.

---
+++ src/infrastructure_infos.R
@@ -0,0 +1,26 @@
+################################################################################
+#
+# INFRASTUKTÚRA INFORMÁCIÓK:
+#
+ITEMS <- c("host-01",
+           "host-02",
+           "guest-11",
+           "guest-13", 
+           "guest-14",
+           "guest-15",
+           "guest-16",
+           "guest-21",
+           "guest-22",
+           "guest-23")
+
+HOSTS <- c("host-01",
+           "host-02")
+
+VMS <- c("guest-11",
+         "guest-13", 
+         "guest-14",
+         "guest-15",
+         "guest-16",
+         "guest-21",
+         "guest-22",
+         "guest-23")
